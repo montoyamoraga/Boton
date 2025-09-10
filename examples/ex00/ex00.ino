d6eb1340827938eb3b5d89b4815dbe52632302fa
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-09-10 00:46:22 -0300

    agregar ejemplo

---
+++ examples/ex00/ex00.ino
@@ -0,0 +1,15 @@
+// include library
+#include "Boton.h"
+
+// create an instance
+Boton boton(3);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
