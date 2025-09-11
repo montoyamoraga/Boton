ca0cf1228f0256314e96cdfee2b61ab20c4f3b5a
aarón montoya-moraga <montoyamoraga@gmail.com>
2025-09-10 21:06:37 -0300

    agregar debounce o desrebotar

--- src/Boton.h
+++ src/Boton.h
@@ -8,12 +8,24 @@ class Boton
 public:
     Boton(uint8_t nuevaPatita);
     void setPatita(uint8_t nuevaPatita);
-    void leer();
+    void actualizar();
     bool getValor();
+
+    // enum Estado
+    // {
+    //     SUELTO,
+    //     PULSANDO,
+    //     PULSADO,
+    //     SOLTANDO
+    // };
 
 private:
     uint8_t patita;
-    bool valorLeido;
+    bool valorLeidoActual;
+    bool valorLeidoAnterior;
+    // Estado estado;
+    unsigned long tiempoAnteriorDesrebotar;
+    unsigned long tiempoEntreRebotes;
 };
 
 #endif
